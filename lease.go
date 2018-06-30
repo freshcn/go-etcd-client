1f715a3d2cc86ad2c7fd86d249e009133a341e50
chenzhouyu <chenzhouyu@qq.com>
2018-07-01 06:13:27 +0800

    租约中的keepalive不适合再次封装，解除其封装

--- lease.go
+++ lease.go
@@ -84,22 +84,6 @@ func (l *Lease) TimeToLive(id int64, opts ...clientv3.LeaseOption) (rep LeaseTim
 	return
 }
 
-// KeepAlive 让租约永远有效
-// 如果租约有效的信息一直没有发送成功，客户端将会每秒发送一次，直到成功为止。
-// 详细说明请查看https://godoc.org/github.com/coreos/etcd/clientv3#Lease
-func (l *Lease) KeepAlive(id int64) (rep LeaseGrantResponse, err error) {
-	var (
-		lrep <-chan *clientv3.LeaseKeepAliveResponse
-	)
-
-	if lrep, err = l.lease.KeepAlive(l.context, clientv3.LeaseID(id)); err == nil {
-		ch := <-lrep
-		rep.ID = int64(ch.ID)
-		rep.TTL = ch.TTL
-	}
-	return
-}
-
 // KeepAliveOnce 为服务设置一次在线保持
 func (l *Lease) KeepAliveOnce(id int64) (rep LeaseGrantResponse, err error) {
 	var (
